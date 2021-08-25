803c827ec9b82e8e135f890c7f425f5a4a834c2e
Liluyoud Cury de Lacerda <liluyoud@outlook.com>
2021-08-24 21:03:28 -0400

    Aula2

---
+++ LojaApp/Pages/Todo.razor.cs
@@ -0,0 +1,25 @@
+﻿using LojaApp.Models;
+namespace LojaApp.Pages;
+
+public partial class Todo
+{
+	private List<TodoModel> todos = new();
+	private string novaTarefa;
+
+	private void AdicionarTarefa()
+	{
+		if (!string.IsNullOrWhiteSpace(novaTarefa))
+		{
+			todos.Add(new TodoModel()
+			{
+				Titulo = novaTarefa
+			});
+			novaTarefa = "";
+		}
+	}
+
+	private string FeitoStyle(bool feito)
+	{
+		return feito ? "font-style: italic;text-decoration: line-through;" : "";
+	}
+}
