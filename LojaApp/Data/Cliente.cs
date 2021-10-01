bc84b9e896a8ebac5b602ac36fb1b01204288d5b
Thaisa Camargo <thaisa.computacao@gmail.com>
2021-10-01 15:18:20 -0400

    X

---
+++ LojaApp/Data/Cliente.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace LojaApp.Data;
+
+public class Cliente
+{
+    /*Criar a tabela de clientes com os seguintes campos: Id, Nome, Cpf, Telefone, Email, Sexo, Estado Civil, 
+     * Escolaridade, Data de Nascimento e Endereço.*/
+    public int Id {  get; set; }
+    public string? Nome {  get; set; }
+    [MaxLength(11)]
+    public int Cpf {  get; set; }
+    [MaxLength(11)]
+    public int Telefone {  get; set; }
+    [Required]
+    public string? Email {  get; set; }
+    public string? Sexo { get; set; }
+    public string? EstadoCivil { get; set; }
+    public string? Escolaridade { get; set; }
+    public string? DataDeNascimento { get; set; }
+
+    [StringLength(255)]
+    public string? Endereco { get; set; }
+
+}
+
